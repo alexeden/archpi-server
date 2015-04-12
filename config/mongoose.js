34584f3015ebd6d129836fa2150ab24caea7b4ed
Alexander Eden <alexandereden91@gmail.com>
2015-04-12 06:13:56 +0000

    new file:   node_modules/connect-mongo/node_modules/debug/component.json sldkfj

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+var	config = require('./modeset'),
+	mongoose = require('mongoose');
+
+
+// Use the Mongoose module and connect to the MongoDB instance using the db property
+module.exports = function() {
+	var db = mongoose.connect(config.db);
+
+	// Implement the Model schemas
+//	require('../app/models/article.server.model');
+	return db;
+}
